316bd3b63deb252af6bb07c6f4a95b90876c2c0f
Charles Williams <C.Williams at gns.cri.nz>
2021-09-23 13:38:00 +1200

    Initial repository for using TDefnode with PyLith for SE Tibet faults.

---
+++ mesh/geometry/create_tdefnode_quads.py
@@ -0,0 +1,471 @@
+#!/usr/bin/env python
+"""
+This is a script to read a Tdefnode .nod file containing fault definitions and
+create Cubit/Trelis journal files to generate the corresponding geometry.
+"""
+
+import pdb
+import numpy
+import math
+from pyproj import Proj
+from pyproj import transform
+from fortranformat import FortranRecordReader
+import pyre.units
+from pyre.units.length import km
+from pyre.units.length import m
+from pyre.units.angle import degree
+
+from pyre.applications.Script import Script as Application
+
+class CreateTdefnodeQuads(Application):
+    """
+    This is a script to read a Tdefnode .nod file containing fault definitions and
+    create Cubit/Trelis journal files to generate the corresponding geometry.
+    """
+
+    import pyre.inventory
+    ## Python object for managing CreateTdefnodeQuads facilities and properties.
+    ##
+    ## \b Properties
+    ## @li \b nod_file Defnode/Tdefnode .nod file to read.
+    ## @li \b extend_file File containing the extension amount for each fault.
+    ## @li \b file_type Defnode or Tdefnode input file.
+    ## @li \b near_surf_eps Points this close will be projected to surface.
+    ## @li \b output_projection Proj4 parameters defining output projection.
+    ## @li \b output_rotation Rotation applied after projection.
+
+    nodFile = pyre.inventory.str("nod_file", default="faults.nod")
+    nodFile.meta['tip'] = "The name of the .nod file to read."
+    
+    extendFile = pyre.inventory.str("extend_file", default="extend.txt")
+    extendFile.meta['tip'] = "File with the extension amount for each fault."
+
+    fileType = pyre.inventory.str("file_type", default="tdefnode",
+                                  validator = pyre.inventory.choice(["defnode", "tdefnode"]))
+    fileType.meta['tip'] = "DEFNODE or TDEFNODE .nod file."
+
+    nearSurfEps = pyre.inventory.dimensional("near_surf_eps",
+                                             default=100.0*m)
+    nearSurfEps.meta['tip'] = "Points this close will be projected to surface."
+
+    outputProjection = pyre.inventory.str("output_projection",
+                                          default="+proj=tmerc +lon_0=175.45 +lat_0=-40.825 +ellps=WGS84 +datum=WGS84 +k=0.9996 +towgs84=0.0,0.0,0.0")
+    outputProjection.meta['tip'] = "Proj4 parameters defining output projection."
+
+    outputRotation = pyre.inventory.dimensional("output_rotation", default=45.0*degree)
+    outputRotation.meta['tip'] = "Rotation applied after projection."
+                                    
+
+    # PUBLIC METHODS /////////////////////////////////////////////////////
+
+    def __init__(self, name="create_tdefnode_quads"):
+        Application.__init__(self, name)
+        self.numFaults = 0
+        self.faultExtendsUpDict = {}
+        self.faultExtendsDownDict = {}
+        self.faultExtendsAsNegDict = {}
+        self.faultExtendsAsPosDict = {}
+
+        self.outProj = None
+        WGS84 = "+proj=lonlat +ellps=WGS84 +datum=WGS84 +towgs84=0.0,0.0,0.0"
+        self.projWGS84 = Proj(WGS84)
+
+        self.charEnd = 10
+
+        self.newLine = "\n"
+        self.separator = "# -----------------------------------------------------\n"
+        self.comment = "#"
+
+        return
+
+
+    def main(self):
+        # pdb.set_trace()
+        self._readExtends()
+        self._readFaults()
+
+        return
+                                    
+
+    # PRIVATE METHODS /////////////////////////////////////////////////////
+
+
+    def _configure(self):
+        """
+        Setup members using inventory.
+        """
+        Application._configure(self)
+        self.outProj = Proj(self.outputProjection)
+        rot = self.outputRotation.value
+        cosR = math.cos(rot)
+        sinR = math.sin(rot)
+        self.rotMat = numpy.array([[ cosR, -sinR, 0.0],
+                                   [ sinR,  cosR, 0.0],
+                                   [0.0, 0.0, 1.0]], dtype=numpy.float64)
+
+        if (self.fileType == "defnode"):
+            nodFmt = "(a10, 3i3, 2(1x,a4), 1x, 3f9.3, 2f10.1, 4f8.1, f8.4," + \
+                " 2f10.1, 6f8.1, 2f10.4, 1pe10.3)"
+            self.nodFmt = FortranRecordReader(nodFmt)
+        else:
+            nodFmt = "(a10, 3i4, 2(1x,a4), 1x, 3f9.3, 2f10.3, 4f8.1, f8.4," + \
+                " 8f8.1, 1x, 1pe10.3)"
+            self.nodFmt = FortranRecordReader(nodFmt)
+
+        return
+    
+
+    def _readExtends(self):
+        """
+        Function to read amount of extension for each fault and create a dict.
+        """
+        f = open(self.extendFile, 'r')
+        lines = f.readlines()
+        faultNames = []
+        faultUpExtends = []
+        faultDownExtends = []
+        faultAsNegExtends = []
+        faultAsPosExtends = []
+        uparser = pyre.units.parser()
+        for lineNum in range(len(lines)):
+            line = lines[lineNum]
+            if (line.startswith('#') == False):
+                faultNames.append(line.split()[0])
+                extendUpAmount = uparser.parse(line.split()[1]).value
+                extendDownAmount = uparser.parse(line.split()[2]).value
+                extendAsNegAmount = uparser.parse(line.split()[3]).value
+                extendAsPosAmount = uparser.parse(line.split()[4]).value
+                faultUpExtends.append(extendUpAmount)
+                faultDownExtends.append(extendDownAmount)
+                faultAsNegExtends.append(extendAsNegAmount)
+                faultAsPosExtends.append(extendAsPosAmount)
+
+        self.faultExtendsUpDict = dict(zip(faultNames, faultUpExtends))
+        self.faultExtendsDownDict = dict(zip(faultNames, faultDownExtends))
+        self.faultExtendsAsNegDict = dict(zip(faultNames, faultAsNegExtends))
+        self.faultExtendsAsPosDict = dict(zip(faultNames, faultAsPosExtends))
+
+        return
+      
+
+    def _readFaults(self):
+        """
+        Function to loop over faults and get/write mesh info for each one.
+        """
+
+        print("Looping over faults:")
+
+        # First find number of faults.
+        f = open(self.nodFile, 'r')
+        lines = f.readlines()
+        numLines = len(lines)
+
+        names = [lines[i][0:self.charEnd] for i in range(numLines)]
+        uniqNames = list(set(names))
+        self.numFaults = len(uniqNames)
+        print("  Number of faults:  %d" % self.numFaults)
+
+        # Loop over faults.
+        for faultNum in range(self.numFaults):
+            lowIndex = names.index(uniqNames[faultNum])
+            highIndex = lowIndex + names.count(uniqNames[faultNum])
+            self._processFault(lines[lowIndex:highIndex])
+            
+        return
+  
+    
+    def _processFault(self, lines):
+        """
+        Function to process a single fault.
+        """
+
+        faultName = lines[0][0:self.charEnd].strip()
+        print("  Working on fault:  %s" % faultName)
+
+        numNodes = len(lines)
+
+        xIndex = numpy.zeros(numNodes, dtype=numpy.int)
+        zIndex = numpy.zeros(numNodes, dtype=numpy.int)
+        lon = numpy.zeros(numNodes, dtype=numpy.float64)
+        lat = numpy.zeros(numNodes, dtype=numpy.float64)
+        elev = numpy.zeros(numNodes, dtype=numpy.float64)
+
+        numAsNodes = 0
+        numDdNodes = 0
+
+        for lineNum in range(numNodes):
+            vals = self.nodFmt.read(lines[lineNum])
+            xIndex[lineNum] = vals[2]
+            zIndex[lineNum] = vals[3]
+            lon[lineNum] = vals[6]
+            lat[lineNum] = vals[7]
+            elev[lineNum] = -1000.0 * vals[8]
+            numAsNodes = max(numAsNodes, xIndex[lineNum])
+            numDdNodes = max(numDdNodes, zIndex[lineNum])
+
+        (x, y, z) = transform(self.projWGS84, self.outProj, lon, lat, elev)
+        coords = numpy.column_stack((x, y, z))
+        coordsRot = numpy.dot(self.rotMat, coords.transpose()).transpose()
+        (coordsExt, numAsNodesExt, numDdNodesExt) = self._createExtCoords(coordsRot, numAsNodes, numDdNodes,
+                                                                          self.faultExtendsUpDict[faultName],
+                                                                          self.faultExtendsDownDict[faultName],
+                                                                          self.faultExtendsAsNegDict[faultName],
+                                                                          self.faultExtendsAsPosDict[faultName])
+
+        numNodesExt = numAsNodesExt * numDdNodesExt
+        vertInds = numpy.arange(1, numNodesExt + 1, step=1, dtype=numpy.int32).reshape((numAsNodesExt, numDdNodesExt))
+
+        # Write VTK file.
+        self._writeVtk(faultName, coordsExt, numNodesExt, numAsNodesExt, numDdNodesExt)
+
+        masterJournal = faultName + '_master.jou'
+        vertexJournal = faultName + '_vertex.jou'
+        surfJournal = faultName + '_surf.jou'
+
+        # Write master journal file.
+        self._writeMasterJournal(faultName, masterJournal, vertexJournal, surfJournal)
+
+        # Write vertex definition journal file.
+        self._writeVertJournal(vertexJournal, coordsExt, numAsNodesExt, numDdNodesExt)
+
+        # Write surface definition journal file.
+        self._writeSurfJournal(surfJournal, vertInds, numAsNodesExt, numDdNodesExt)
+
+        return
+
+
+    def _writeSurfJournal(self, surfJournal, vertInds, numAsNodes, numDdNodes):
+        """
+        Function to create Tdefnode quadrilaterals from vertices.
+        """
+
+        s = open(surfJournal, 'w')
+        surfFmt = "create surface curve %d %d %d %d\n"
+        curveFmt = "create curve vertex %d %d\n"
+        cNum = 0
+        curveInds = numpy.zeros((numAsNodes -1, numDdNodes - 1, 4), dtype=numpy.int32)
+
+        # First create curves along strike.
+        for asNode in range(numAsNodes - 1):
+            cNum += 1
+            for ddNode in range(numDdNodes - 1):
+                v1 = vertInds[asNode, ddNode]
+                v2 = vertInds[asNode + 1, ddNode]
+                s.write(curveFmt % (v1, v2))
+                if (ddNode == numDdNodes - 2):
+                    v1 = vertInds[asNode, ddNode + 1]
+                    v2 = vertInds[asNode + 1, ddNode + 1]
+                    s.write(curveFmt % (v1, v2))
+                curveInds[asNode, ddNode, 0] = cNum
+                curveInds[asNode, ddNode, 2] = cNum + 1
+                cNum += 1
+
+        # Create curves down dip.
+        for ddNode in range(numDdNodes - 1):
+            cNum += 1
+            for asNode in range(numAsNodes - 1):
+                v1 = vertInds[asNode, ddNode]
+                v2 = vertInds[asNode, ddNode + 1]
+                s.write(curveFmt % (v1, v2))
+                if (asNode == numAsNodes - 2):
+                    v1 = vertInds[asNode + 1, ddNode]
+                    v2 = vertInds[asNode + 1, ddNode + 1]
+                    s.write(curveFmt % (v1, v2))
+                curveInds[asNode, ddNode, 1] = cNum
+                curveInds[asNode, ddNode, 3] = cNum + 1
+                cNum += 1
+        
+        # Create surfaces with straight line curves.
+        for asNode in range(numAsNodes - 1):
+            for ddNode in range(numDdNodes - 1):
+                c1 = curveInds[asNode, ddNode, 0]
+                c2 = curveInds[asNode, ddNode, 1]
+                c3 = curveInds[asNode, ddNode, 2]
+                c4 = curveInds[asNode, ddNode, 3]
+                s.write(surfFmt % (c1, c2, c3, c4))
+
+        s.close()
+
+        return
+    
+    
+    def _writeVertJournal(self, vertexJournal, coords, numAsNodes, numDdNodes):
+        """
+        Function to write vertices defining Tdefnode quads.
+        """
+
+        v = open(vertexJournal, 'w')
+        vertFmt = "create vertex %15.11e %15.11e %15.11e\n"
+
+        for row in range(numAsNodes):
+            for column in range(numDdNodes):
+                v.write(vertFmt % (coords[row, column, 0], coords[row, column, 1], coords[row, column, 2]))
+
+
+        v.close()
+
+        return
+  
+    
+    def _writeMasterJournal(self, faultName, masterJournal, vertexJournal, surfJournal):
+        """
+        Function to write the master journal file for geometry creation.
+        """
+
+        m = open(masterJournal, 'w')
+
+        # Playback other parts.
+        playCmd = \
+          "reset\n" + \
+          "# This is a simple Cubit journal file to create a volume\n" + \
+          "# containing a set of quadrilateral surfaces from a set of\n" + \
+          "# DEFNODE vertices.\n" + \
+          self.comment + self.newLine + self.separator + \
+          "# Create vertices, then quadrilaterals, then curves to define\n" + \
+          "# a surrounding volume.\n" + \
+          self.separator + \
+          "playback " + "'" + vertexJournal + "'" + "\n" + \
+          "playback " + "'" + surfJournal + "'" + "\n"
+
+        m.write(playCmd)
+
+        # Imprint and merge.
+        comment1 = self.comment + self.newLine + self.separator + \
+          "# Imprint and merge all surfaces.\n" + self.separator
+        m.write(comment1)
+        # imprintCmd = "imprint all\n" + \
+          #              "merge all\n"
+        mergeCmd = "merge all\n"
+        m.write(mergeCmd)
+
+        # Export surfaces.
+        acisFilename = faultName + "_acis.sat"
+        cubitFilename = faultName + "_cubit.cub"
+        comment2 = self.comment + self.newLine + self.separator + \
+          "# Export DEFNODE volumes.\n" + self.separator
+        m.write(comment2)
+        exportAcis = "export Acis '" + acisFilename + "' surface all overwrite\n"
+        exportCubit = "export Cubit '" + cubitFilename + "' surface all overwrite\n"
+        m.write(exportAcis)
+        m.write(exportCubit)
+        m.close()
+    
+        return
+    
+    
+    def _createExtCoords(self, coords, numAsNodes, numDdNodes, upExt, downExt, asNegExt, asPosExt):
+        """
+        Function to create extended coordinates array.
+        """
+
+        zMax = numpy.amax(coords[:,2])
+        numAsNodesExt = numAsNodes + 2
+        numDdNodesExt = numDdNodes + 2
+        topNode = 1
+        coordsReshape = coords.reshape((numAsNodes, numDdNodes, 3), order='F')
+        coordsExt = numpy.zeros((numAsNodesExt, numDdNodesExt, 3))
+
+        # Extend updip, if possible.
+        top = coordsReshape[:,0,:]
+        atSurface = numpy.allclose(top[:,2], 0.0, atol=self.nearSurfEps.value)
+
+        if (atSurface):
+            numDdNodesExt = numDdNodes + 1
+            topNode = 0
+            coordsExt = numpy.zeros((numAsNodesExt, numDdNodesExt, 3))
+            coordsExt[1:-1,topNode:-1,:] = coordsReshape
+            coordsExt[:,topNode,2] = self.nearSurfEps.value
+        else:
+            coordsExt[1:-1,topNode:-1,:] = coordsReshape
+            diff = coordsReshape[:,0,:] - coordsReshape[:,1,:]
+            diffNorm = numpy.linalg.norm(diff, axis=1)
+            diffVec = diff/diffNorm.reshape((numAsNodes, 1))
+            testTop = coordsReshape[:,topNode,:] + upExt * diffVec
+            coordsExt[1:-1,0,:] = testTop
+            aboveSurf = numpy.any(testTop[:,2] >= 0.0)
+            if (aboveSurf):
+                coordsExt[1:-1,0,2] = self.nearSurfEps.value
+
+        # Extend downdip.
+        diff = coordsReshape[:,-1,:] - coordsReshape[:,-2,:]
+        diffNorm = numpy.linalg.norm(diff, axis=1)
+        diffVec = diff/diffNorm.reshape((numAsNodes, 1))
+        bottom = coordsReshape[:,-1,:] + downExt * diffVec
+        coordsExt[1:-1,-1,:] = bottom
+
+        # Extend left.
+        diff = coordsExt[1,:,:] - coordsExt[2,:,:]
+        diffNorm = numpy.linalg.norm(diff, axis=1)
+        diffVec = diff/diffNorm.reshape((numDdNodesExt, 1))
+        left = coordsExt[1,:,:] + asNegExt * diffVec
+        coordsExt[0,:,:] = left
+
+        # Extend right.
+        diff = coordsExt[-2,:,:] - coordsExt[-3,:,:]
+        diffNorm = numpy.linalg.norm(diff, axis=1)
+        diffVec = diff/diffNorm.reshape((numDdNodesExt, 1))
+        right = coordsExt[-2,:,:] + asPosExt * diffVec
+        coordsExt[-1,:,:] = right
+
+        return (coordsExt, numAsNodesExt, numDdNodesExt)
+      
+    
+    def _writeVtk(self, faultName, coords, numNodes, numAsNodes, numDdNodes):
+        """
+        Function to write coordinates and cell connectivity info.
+        """
+
+        fileName = faultName + '.vtk'
+        v = open(fileName, 'w')
+        # VTK headers.
+        headerTop = "# vtk DataFile Version 2.0\n" + \
+            "Extended fault geometry from DEFNODE\n" + \
+            "ASCII\n" + \
+            "DATASET UNSTRUCTURED_GRID\n"
+        headerTotal = headerTop + \
+            "POINTS " + repr(numNodes) + " double\n"
+        v.write(headerTotal)
+
+        coordsOut = coords.reshape((numNodes, 3))
+        numpy.savetxt(v, coordsOut)
+    
+        # Determine connectivity.
+        numCells = (numAsNodes - 1) * (numDdNodes - 1)
+        numCellEntries = 5 * numCells
+        headConnect = "CELLS %d %d\n" % (numCells, numCellEntries)
+        connect = numpy.zeros((numCells, 5), dtype=numpy.int32)
+        connect[:,0] = 4
+        headCellType = "CELL_TYPES %d\n" % numCells
+        cellType = 9 * numpy.ones(numCells, dtype=numpy.int32)
+        intFmt = "%d"
+
+        cellNum = 0
+        for dipCell in range(numDdNodes - 1):
+            for strikeCell in range(numAsNodes - 1):
+                v1 = dipCell + strikeCell * numDdNodes
+                v2 = v1 + 1
+                v3 = v2 + numDdNodes
+                v4 = v1 + numDdNodes
+                connect[cellNum,1] = v1
+                connect[cellNum,2] = v2
+                connect[cellNum,3] = v3
+                connect[cellNum,4] = v4
+                cellNum += 1
+
+        # Write connectivity info.
+        v.write(headConnect)
+        numpy.savetxt(v, connect, fmt=intFmt)
+        v.write(headCellType)
+        numpy.savetxt(v,cellType, fmt=intFmt)
+
+        v.close()
+
+        return
+    
+    
+# ----------------------------------------------------------------------
+if __name__ == '__main__':
+    app = CreateTdefnodeQuads()
+    app.run()
+
+# End of file
