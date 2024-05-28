7aa2523f28e8fa5a0d5204430f53dfc84a6dd308
Charles Williams <C.Williams at gns.cri.nz>
2024-05-28 15:56:55 +1200

    Deleted unneeded fault-normal .cfg and .sh files.

--- examples/daliangshan/pylith/config-v2/run_lij_nm_homog_cluster.sh
+++
@@ -1,1 +0,0 @@
-pylith solver_fs1.cfg gf_lijiang_nm_homog.cfg --problem=pylith.problems.GreensFns --nodes=48 pylithapp_openmpi_pbs_np48_eth0.cfg --job.name=gf_lijiang_nm_homog --job.stdout=gf_lijiang_nm_homog.log --job.stderr=gf_lijiang_nm_homog.err
--- examples/daliangshan/pylith/config-v2/run_xsh_nm_heter_cluster.sh
+++
@@ -1,1 +0,0 @@
-pylith solver_fs1.cfg gf_xsh_nm_heter.cfg --problem=pylith.problems.GreensFns --nodes=48 pylithapp_openmpi_pbs_np48_eth0.cfg --job.name=gf_xsh_nm_heter --job.stdout=gf_xsh_nm_heter.log --job.stderr=gf_xsh_nm_heter.err
